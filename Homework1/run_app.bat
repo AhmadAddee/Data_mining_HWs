3df40586fe1d138052dbcac08f99b07ec4890d16
alkha <al.khateeb.ahmad96@gmail.com>
2025-11-10 04:03:47 +0100

    Add benchmark command

--- Homework1/run_app.bat
+++ Homework1/run_app.bat
@@ -2,9 +2,9 @@
 REM Change these paths before running
 set FILE_PATH=<replace with path to your zip file>
 set num_of_docs=100
+set SIGLEN=256
 set k=10
-set SIGLEN=128
 set THRESH=0.8
 
-python main.py --zipfile "%FILE_PATH%" --num-of-docs %num_of_docs% --shingle-length %k% --signature-length %SIGLEN% --threshold %THRESH%
+python main.py --zipfile "%FILE_PATH%" --num-of-docs %num_of_docs% --benchmark --grid-k %k% --grid-siglen %SIGLEN% --grid-threshold %THRESH% --grid-bands auto --sizes 5,10,20,40,80,160,320,740,1000 --repeat 1 --plot-out runtime.png
 pause
