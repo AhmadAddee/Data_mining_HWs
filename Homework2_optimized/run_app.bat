c2b87c484711bdbecb60c8bcb37564b5c1f8941f
alkha <al.khateeb.ahmad96@gmail.com>
2025-11-18 14:29:55 +0100

    Removed local path

--- Homework2_optimized/run_app.bat
+++ Homework2_optimized/run_app.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Change these paths before running
-set FILE_PATH=C:\Skolan\TSEDM1\Data mining ID2222\Data_mining_HWs\Homework2\dataset\T10I4D100K.dat
+set FILE_PATH=<replace with path to your data file>
 set SUPPORT=1000
 set CONFIDENCE=0.5
 
